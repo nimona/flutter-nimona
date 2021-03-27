c0447c8ac4fc8634e4a3753732f7243982afcbf4
George Antoniadis <george@noodles.gr>
2021-03-27 20:59:01 +0000

    feat: add windows skeleton

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <nimona/nimona_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  NimonaPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NimonaPlugin"));
+}
